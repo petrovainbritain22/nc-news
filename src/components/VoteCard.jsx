74d7c8fd6210d75d1c32ba7922c64ccae622f290
petrovainbritain22 <petrova7viktoriia@gmail.com>
2022-09-29 09:20:59 +0100

    ticket #7 - vote on article

--- src/components/VoteCard.jsx
+++ src/components/VoteCard.jsx
@@ -1,9 +1,37 @@
+import {useEffect, useState} from "react";
+import {patchArticleVotes} from "../utils/api";
+
 export default function VoteCard({votes, article_id}) {
+  const [newVotes, setNewVotes] = useState(votes);
+
+  const [errMsg, setErrMsg] = useState(undefined);
+  const voteUpHandler = (e) => {
+    updateArticleVote({inc_votes: 1});
+  };
+  const voteDownHandler = (e) => {
+    updateArticleVote({inc_votes: -1});
+  };
+  const updateArticleVote = (vote) => {
+    patchArticleVotes(article_id, vote)
+      .then(({article}) => {
+        setNewVotes((currVotes) => {
+          return currVotes + vote.inc_votes;
+        });
+      })
+      .catch((err) => {
+        setErrMsg(err.response.data.msg);
+      });
+  };
+  useEffect(() => {
+    setErrMsg(undefined);
+  }, [newVotes]);
+
   return (
     <p>
-      <button>Up</button>
-      {votes} votes
-      <button>Down</button>
+      <button onClick={voteUpHandler}>Up</button>
+      {newVotes} votes
+      <button onClick={voteDownHandler}>Down</button>
+      <span>{errMsg ? errMsg : null}</span>
     </p>
   );
 }
